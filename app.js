ab57cec6f49521a70368c4dc213aef4bb7ca7714
MARIA <raphael.maria@vws.vh20.net>
2020-09-28 16:21:31 +0200

    Adressed separation of concerns

--- app.js
+++ app.js
@@ -5,86 +5,17 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 
+
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(methodOverride('_method'))
 
-// OUR MOCK ARRAY OF PROJECTS
-// let reviews = [
-//     { title: "Great Review", movieTitle: "Batman II" },
-//     { title: "Awesome Movie", movieTitle: "Titanic" }
-// ]
-  
+  const reviews = require('./controllers/reviews')(app);
 
 
-const Review = mongoose.model('Review', {
-    title: String,
-    description: String,
-    movieTitle: String
-  });
-  // INDEX
-  app.get('/', (req, res) => {
-    Review.find()
-      .then(reviews => {
-        res.render('reviews-index', { reviews: reviews });
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  })
 
-// NEW
-app.get('/reviews/new', (req, res) => {
-    res.render('reviews-new', {title: "New Review"});
-})
-
-// SHOW
-app.get('/reviews/:id', (req, res) => {
-    Review.findById(req.params.id).then((review) => {
-      res.render('reviews-show', { review: review })
-    }).catch((err) => {
-      console.log(err.message);
-    })
-  })
-
-// CREATE
-app.post('/reviews', (req, res) => {
-    Review.create(req.body).then((review) => {
-      console.log(review)
-      res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
-    }).catch((err) => {
-      console.log(err.message)
-    })
-  })
-// EDIT
-app.get('/reviews/:id/edit', (req, res) => {
-    Review.findById(req.params.id, function(err, review) {
-      res.render('reviews-edit', {review: review, title: "Edit Review"});
-    })
-  })
-// UPDATE
-app.put('/reviews/:id', (req, res) => {
-    Review.findByIdAndUpdate(req.params.id, req.body)
-      .then(review => {
-        res.redirect(`/reviews/${review._id}`)
-      })
-      .catch(err => {
-        console.log(err.message)
-      })
-  })
-app.delete('/reviews/:id', function (req, res) {
-    console.log("DELETE review")
-    Review.findByIdAndRemove(req.params.id).then((review) => {
-      res.redirect('/');
-    }).catch((err) => {
-      console.log(err.message);
-    })
-  })
-app.listen(3000, () => {
-    console.log('App listening on port 3000!')
-})
 
 var exphbs = require('express-handlebars');
 
