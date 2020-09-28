06eb7156aaa9a4bbc9664728a3e48a310fa34688
MARIA <raphael.maria@vws.vh20.net>
2020-09-28 16:04:09 +0200

    Users can edit and update reviews

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const methodOverride = require('method-override')
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(methodOverride('_method'))
 
 // OUR MOCK ARRAY OF PROJECTS
 // let reviews = [
@@ -30,9 +34,12 @@ const Review = mongoose.model('Review', {
         console.log(err);
       })
   })
-  app.get('/reviews/new', (req, res) => {
-    res.render('reviews-new', {});
-  })
+
+// NEW
+app.get('/reviews/new', (req, res) => {
+    res.render('reviews-new', {title: "New Review"});
+})
+
 // SHOW
 app.get('/reviews/:id', (req, res) => {
     Review.findById(req.params.id).then((review) => {
@@ -41,7 +48,6 @@ app.get('/reviews/:id', (req, res) => {
       console.log(err.message);
     })
   })
-  // NEW
 
 // CREATE
 app.post('/reviews', (req, res) => {
@@ -51,6 +57,22 @@ app.post('/reviews', (req, res) => {
     }).catch((err) => {
       console.log(err.message)
     })
+  })
+// EDIT
+app.get('/reviews/:id/edit', (req, res) => {
+    Review.findById(req.params.id, function(err, review) {
+      res.render('reviews-edit', {review: review, title: "Edit Review"});
+    })
+  })
+// UPDATE
+app.put('/reviews/:id', (req, res) => {
+    Review.findByIdAndUpdate(req.params.id, req.body)
+      .then(review => {
+        res.redirect(`/reviews/${review._id}`)
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
   })
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
