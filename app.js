58fa05b42fd5271e6e1f7ab4cba9c2ef1f9d50c4
MARIA <raphael.maria@vws.vh20.net>
2020-09-29 11:21:50 +0200

    Added tests

--- app.js
+++ app.js
@@ -4,20 +4,17 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-
-
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(methodOverride('_method'))
 
-  const reviews = require('./controllers/reviews')(app);
-
-
-
+const reviews = require('./controllers/reviews')(app);
 
 var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+module.exports = app;
